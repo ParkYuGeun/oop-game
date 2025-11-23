24f7173a0f888fc7aac2577be3bbc3138aa42656
qnrPwjd <bugyejung21234@gmail.com>
2025-11-23 15:49:02 +0900

    Play, Exit GameButton통합
    Play, Exit GameButton 메서드 통합

--- My project/Assets/Blademan Survival/Codes/PlayExitGameButton.cs
+++ My project/Assets/Blademan Survival/Codes/PlayExitGameButton.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 // This class is responsible for handling the Play Game button functionality.
 using UnityEngine.SceneManagement;
 
-public class PlayGameButton : MonoBehaviour
+public class PlayExitGameButton : MonoBehaviour
 {
+    public void OnExitGameButtonClicked()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
     public void OnPlayGameButtonClicked()
     {
         // Load the main game scene when the Play Game button is clicked.
